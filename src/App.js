2f204b3756fdf6850668616c6d01e1090057d74f
THOMAS HARDING <hardingt604@gmail.com>
2022-05-05 12:23:39 -0600

    add component calls, required state, required functoins to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'React';
+import MovieForm from './MovieForm';
+import MovieList from './MovieList';
+import Movie from './Movie';
 import './App.css';
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setFilteredMovies] = useState([]); 
+  const [movieFormYearReleased, setMovieFormYearReleased] = useState(''); 
+  const [movieFormDirector, setMovieFormDirector] = useState(''); 
+  const [movieFormTitle, setMovieFormTitle] = useState(''); 
+  const [movieFormColor, setMovieFormColor] = useState('');
+
+  function handleDeleteMovie(title) {
+    const index = allMovies.findIndex(movie => movie.title === title);
+    allMovies.splice(index, 1);
+    setAllMovies([...allMovies]);
+  }
+
+  function handleSubmitMovie(e) {
+    e.preventDefault();
+    let newMovie = { title: movieFormTitle, color: movieFormColor, director: movieFormDirector };
+    setAllMovies([...allMovies, newMovie]);
+    setMovieFormColor('');
+    setMovieFormYearReleased('');
+    setMovieFormDirector('');
+    setMovieFormTitle('');
+  }
+  function handleFilterMovies(search) {
+    const filtered = allMovies
+      .filter(movie => movie.name.toLowerCase().includes(search.toLowerCase()));
+    if (search) {
+      setFilteredMovies(filtered);
+    } else {
+      setFilteredMovies(allMovies);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='movieForm'>
+        <MovieForm
+          setMovieFormName={setMovieFormDirector}
+          movieFormYearReleased={movieFormYearReleased}
+          setMovieFormTitle={movieFormDirector}
+          setMovieFormColor={setMovieFormColor}
+          movieFormColor={movieFormColor}
+          handleSubmitMovie={handleSubmitMovie}
+        />
+      </div>
+      <div className='filter'>
+        Filter Movies
+        <input onChange={(e) => handleFilterMovies(e.target.value)} />
+      </div>
+      <MovieList movies={(filteredMovies.length !== 0) ? filteredMovies : allMovies} />
     </div>
   );
 }
